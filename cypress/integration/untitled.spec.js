307d138847dea2f4e6bc4db2881e70d89d32460f
0x0ca5aaa5f0c9217e6f45fe1d109c24fa <78989047+0x0ca5aaa5f0c9217e6f45fe1d109c24fa@users.noreply.github.com>
2022-03-31 17:45:38 +0200

    Update untitled.spec.js

--- cypress/integration/untitled.spec.js
+++ cypress/integration/untitled.spec.js
@@ -23,7 +23,7 @@ describe('My First Test', () => {
                     'user-agent': 'Mozilla/5.0 ' + Math.random(),
                 }
             })
-
+            /*
             cy.wait(20000 + Math.random() * 2000)
             
             cy.visit('https://soundcloud.com/monomente-podcast/kathi-boehmer-034', {
