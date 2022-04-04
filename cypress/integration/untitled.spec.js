86f156a93fd22e8825fef7ca620de17da61de205
0x0ca5aaa5f0c9217e6f45fe1d109c24fa <78989047+0x0ca5aaa5f0c9217e6f45fe1d109c24fa@users.noreply.github.com>
2022-04-05 00:24:12 +0200

    Update untitled.spec.js

--- cypress/integration/untitled.spec.js
+++ cypress/integration/untitled.spec.js
@@ -6,42 +6,14 @@
 // https://on.cypress.io/writing-first-test
 describe('My First Test', () => {
     it('Does not do much!', () => {
-        expect(true).to.equal(true)
-
-        for (let i = 0; i <= 74; i++) {
-            cy.visit('https://soundcloud.com/monomente-podcast/goldgrund-037', {
+        for (let i = 0; i <= 50; i++) {
+            cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
                 }
             })
 
             cy.wait(58000 + Math.random() * 4000)
-
-            /*
-            cy.visit('https://soundcloud.com/monomente-podcast/you-and-another-003', {
-                headers: {
-                    'user-agent': 'Mozilla/5.0 ' + Math.random(),
-                }
-            })
-            /*
-            cy.wait(20000 + Math.random() * 2000)
-            
-            cy.visit('https://soundcloud.com/monomente-podcast/kathi-boehmer-034', {
-                headers: {
-                    'user-agent': 'Mozilla/5.0 ' + Math.random(),
-                }
-            })
-
-            cy.wait(10000 + Math.random() * 2000)
-            
-            cy.visit('https://soundcloud.com/monomente-podcast/johannes-frank-033', {
-                headers: {
-                    'user-agent': 'Mozilla/5.0 ' + Math.random(),
-                }
-            })
-
-            cy.wait(20000 + Math.random() * 2000)
-            */
         }
     })
 })
