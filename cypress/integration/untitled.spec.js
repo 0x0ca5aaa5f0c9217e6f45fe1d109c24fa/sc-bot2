519a7832efe1b0c79c0777faf9d92d978f0a8726
0x0ca5aaa5f0c9217e6f45fe1d109c24fa <78989047+0x0ca5aaa5f0c9217e6f45fe1d109c24fa@users.noreply.github.com>
2022-04-07 15:16:03 +0200

    Update untitled.spec.js

--- cypress/integration/untitled.spec.js
+++ cypress/integration/untitled.spec.js
@@ -1,7 +1,7 @@
 describe('My First Test', () => {
     it('Does not do much!', () => {
         for (let i = 0; i <= 1000; i++) {
-            cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-2', {
+           /* cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-2', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
                     'referrer': 'https://www.twitter.com',
@@ -18,7 +18,7 @@ describe('My First Test', () => {
             })
 
             cy.wait(20000 + Math.random() * 2000)
-
+*/
             cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
@@ -26,7 +26,7 @@ describe('My First Test', () => {
                 }
             })
 
-            cy.wait(20000 + Math.random() * 2000)
+            cy.wait(60000 + Math.random() * 2000)
         }
     })
 })
