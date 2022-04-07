05d686f6ba22e633c0dff5fde0cf4dfb517a1081
0x0ca5aaa5f0c9217e6f45fe1d109c24fa <78989047+0x0ca5aaa5f0c9217e6f45fe1d109c24fa@users.noreply.github.com>
2022-04-07 15:09:46 +0200

    Update untitled.spec.js

--- cypress/integration/untitled.spec.js
+++ cypress/integration/untitled.spec.js
@@ -1,15 +1,32 @@
-
 describe('My First Test', () => {
     it('Does not do much!', () => {
-        for (let i = 0; i <= 87; i++) {
-            cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
+        for (let i = 0; i <= 1; i++) {
+            cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-2', {
                 headers: {
-                    'user-agent': 'Opera/7.1 ' + Math.random() * 6,
-                    'referrer': 'https://www.nyc.com',
+                    'user-agent': 'Opera/7.0 ' + Math.random(),
+                    'referrer': 'https://www.twitter.com',
                 }
             })
 
-            cy.wait(58000 + Math.random() * 5000)
+            cy.wait(20000 + Math.random() * 2000)
+
+            cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-1', {
+                headers: {
+                    'user-agent': 'Opera/7.0 ' + Math.random(),
+                    'referrer': 'https://www.twitter.com',
+                }
+            })
+
+            cy.wait(20000 + Math.random() * 2000)
+
+            cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
+                headers: {
+                    'user-agent': 'Opera/7.0 ' + Math.random(),
+                    'referrer': 'https://www.twitter.com',
+                }
+            })
+
+            cy.wait(20000 + Math.random() * 2000)
         }
     })
 })
