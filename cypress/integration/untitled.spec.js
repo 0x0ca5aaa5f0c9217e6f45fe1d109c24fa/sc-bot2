35afdfc642aa482bbba1b61171689f3c8f575335
0x0ca5aaa5f0c9217e6f45fe1d109c24fa <78989047+0x0ca5aaa5f0c9217e6f45fe1d109c24fa@users.noreply.github.com>
2022-04-14 01:43:48 +0200

    Update untitled.spec.js

--- cypress/integration/untitled.spec.js
+++ cypress/integration/untitled.spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
     it('Does not do much!', () => {
-        for (let i = 0; i <= 47; i++) {
+        for (let i = 0; i <= 470; i++) {
             
             cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
                 headers: {
